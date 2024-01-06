251a9c7d074691acf3b5ab7ce0a39a23ef3c2d85
akhil <101108396+akhil0203@users.noreply.github.com>
2024-01-06 15:37:18 +0530

    Add files via upload

---
+++ src/Components/About.jsx
@@ -0,0 +1,87 @@
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+import React from "react";
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/shapelined-_JBKdviweXI-unsplash.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "I'm a software developer passionate about frontend, expanding into full-stack. I thrive on continual learning, providing tech solutions for real-world needs.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "💻 Programming: C++, Python, HTML, CSS, JavaScript",
+  "🌐 Web Development: React",
+  "📊 Data Visualization: Tableau",
+  "📝 Project Management: Jira",
+  "📚 Computer Science: Data Structures and Algorithms, Operating Systems",
+  "🌱 Version Control: Git, GitHub",
+  "🗃️ Database: SQL",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  " ";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>Who Am I ❓ </h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 1,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,76 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "/src/images/lukas-blazek-GnvurwJsKaY-unsplash.jpg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Taskminder 📅",
+    description:
+      "A web application developed using React for the frontend and a JSON-based database for managing tasks. This project facilitates creating, reading, updating, and deleting tasks through an intuitive user interface.",
+    url: "https://github.com/akhil0203/taskminder",
+  },
+  {
+    title: "Portfolio Site 🌐",
+    description:
+      "This is my personal portfolio site, showcasing my skills, projects, and experiences in software development. Developed with React and GitHub Codespaces.",
+    url: "https://github.com/akhil0203/portfolio",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <div className="boxhover" key={project.title}>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                </a>
+                <p className="small">{project.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
